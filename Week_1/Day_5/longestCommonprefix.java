5442e3a9dceebba7eefbc053dcf5570d62365918
shreyatrivedi20 <trivedishreya317@gmail.com>
2026-06-05 12:53:30 +0530

    Completed day-5 problems

---
+++ Week_1/Day_5/longestCommonprefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+       Arrays.sort(strs); 
+       StringBuilder result=new StringBuilder();
+       char[]  first=strs[0].toCharArray();
+       char[]  last=strs[strs.length-1].toCharArray();
+       for(int i=0;i<first.length;i++){
+        if(first[i]!=last[i]){
+            break; 
+        }
+        else{
+            result.append(first[i]);
+        }
+       }
+       return result.toString();
+    }
+}
