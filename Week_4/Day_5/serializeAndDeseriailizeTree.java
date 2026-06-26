8e1ee6988fce478b60efb02ce56d5cd7e4220f59
shreyatrivedi20 <trivedishreya317@gmail.com>
2026-06-26 18:37:56 +0530

    Completed week-4 day-5 problems

---
+++ Week_4/Day_5/serializeAndDeseriailizeTree.java
@@ -0,0 +1,13 @@
+class Solution {
+     private boolean check( TreeNode l , TreeNode r){
+        if(l == null && r == null) return true;
+        if(l == null || r == null) return false;
+        if( l.val != r.val) return false;
+       
+        return check(l.left , r.right) && check(l.right , r.left);
+     }
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return check( root.left ,  root.right);
+    }
+}
